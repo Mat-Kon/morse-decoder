ef38679ee5ad1ceb0a15f549ad52a4a69eecd3c8
Mat-Kon <mat_kon@mail.ru>
2023-01-23 14:34:17 +0300

    feat: finished the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let interArray = [];
+    let decLetter = [];
+    let result = [];
+
+    for (let i = 0; i < expr.length; i++ ){
+        interArray.push(expr[i]);
+
+        while(interArray.length === 10){
+            decLetter.push(interArray.splice(0,10).join(''));
+        } 
+    }
+
+    for (let letter of decLetter){
+        if (letter === '**********'){
+            letter = ' ';
+        }
+        letter = letter.replace(/^0+/, '');
+        letter = letter.replace(/10/g, '.');
+        letter = letter.replace(/11/g, '-');
+        
+        for(let key in MORSE_TABLE){
+            
+            if (letter === key){
+                result.push(MORSE_TABLE[key]);
+            };
+
+            if (letter === ' ') {
+                result.push(' ');
+                break;
+            }
+        }
+    }
+    return result.join('');
 }
 
 module.exports = {
